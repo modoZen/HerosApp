aefbd549d4fdf70235d61359c4327a0d632e49af
Max <mdiazn@uni.pe>
2022-11-01 21:38:19 -0500

    primer router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import { HeroesApp } from './HeroesApp'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HeroesApp />
-  </React.StrictMode>
+  <BrowserRouter>
+      <React.StrictMode>
+        <HeroesApp />
+      </React.StrictMode>
+  </BrowserRouter>
 )
