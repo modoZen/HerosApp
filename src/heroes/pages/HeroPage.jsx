a2df291647e179251b55ad32692d59136626a99a
Max <mdiazn@uni.pe>
2022-11-06 17:49:25 -0500

    argumentos URL

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,7 +1,22 @@
+import { Navigate, useParams } from "react-router-dom"
+import { getHeroById } from "../helpers/getHeroById";
 
 export const HeroPage = () => {
+
+  const { id } = useParams();
+
+   const heroById = getHeroById( id );
+
+  if( !heroById ) {
+    return <Navigate to={ -1 } /> 
+  }
+
+
+
   return (
-    <h1>HeroPage</h1>
+    <>
+      <h1>{ heroById.superhero }</h1>
+    </>
   )
 }
 
