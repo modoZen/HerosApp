c47ca56f3eb8a781819633acf85cf37aa75909d1
Max <mdiazn@uni.pe>
2022-11-06 22:03:37 -0500

    SearchComponent

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,6 +1,21 @@
+import { useSearchParams } from "react-router-dom"
+import { useForm } from "../../hooks/useForm"
 import { HeroCard } from "../components/HeroCard"
 
 export const SearchPage = () => {
+
+    const [searchParams, setSearchParams ] = useSearchParams();
+
+    const { searchText, onInputChange, onResetForm } = useForm({
+      searchText: ''
+    })
+
+    const onSearchSubmit = (e)=>{
+      e.preventDefault();
+      if( searchText.trim().length <= 1 ) return;
+      setSearchParams({q: searchText});
+    }
+
     return (
       <>
         <h1>SearchPage</h1>
@@ -10,16 +25,18 @@ export const SearchPage = () => {
           <div className="col-5">
             <h4>Searching...</h4>
             <hr />
-            <form >
+            <form onSubmit={ onSearchSubmit } >
               <input 
                 type="text"
                 placeholder="Search a hero"
                 className="form-control"
                 name="searchText" 
                 autoComplete="off"
+                value={searchText}
+                onChange={ onInputChange }
               />
 
-              <button className="btn btn-outline-primery mt-1">
+              <button className="btn btn-outline-primary mt-2">
                 Search
               </button>
             </form>
@@ -34,7 +51,7 @@ export const SearchPage = () => {
             </div>
 
             <div className="alert alert-danger">
-              No hero with <b>ABC</b>   
+              No hero with <b>{ searchParams.get('q') }</b>   
             </div>
 
             {/* <HeroCard /> */}
