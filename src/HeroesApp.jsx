bf4240294cfe0f577e71358fbcb781ea2718a350
Max <mdiazn@uni.pe>
2022-11-09 23:29:10 -0500

    Context y Reducer

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,9 +1,12 @@
+import { AuthProvider } from "./auth";
 import { AppRouter } from "./routes/AppRouter";
 
 export const HeroesApp = () => {
   return (
     <>
+      <AuthProvider>
         <AppRouter />
+      </AuthProvider>
     </>
   )
 }
---
+++ src/auth/context/authReducer.js
@@ -0,0 +1,21 @@
+import { types } from "../types/types";
+
+export const authReducer = ( state = {}, action ) => {
+    switch( action.type ){
+        case types.login :
+            return {
+                logged : true,
+                name: action.payload
+            }
+        break;
+
+        case types.logout:
+            return {
+                logged: false
+            };
+        break;
+
+        default:
+            return state;
+    }
+}
