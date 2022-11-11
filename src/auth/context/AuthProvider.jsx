a63df19454519dcd8c3baff1821cc5f6a81d9588
Max <mdiazn@uni.pe>
2022-11-10 22:55:12 -0500

    mantener usuario

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -3,22 +3,37 @@ import { types } from "../types/types";
 import { AuthContext } from "./AuthContext";
 import { authReducer } from "./authReducer";
 
-const initialState = {
-    logged: false,
+// const initialState = {
+//     logged: false,
+// };
+
+const init = ()=>{
+    const user = JSON.parse( localStorage.getItem('user') );
+
+    return {
+        logged: !!user,
+        user: user
+    }
 }
 
 export const AuthProvider = ( { children } ) =>{
-    const [ authState, dispatch ] =  useReducer( authReducer, initialState );
+    const [ authState, dispatch ] =  useReducer( authReducer, {} , init );
 
     const login = ( name = '') => {
+        const user = {
+            id: 1,
+            name
+        }
+
         const action = {
             type: types.login,
-            payload: {
-                id: 1,
-                name
-            }
+            payload: user,
         }
-        dispatch( action )
+
+        localStorage.setItem( 'user', JSON.stringify( user ) );
+
+        dispatch( action );
+
     }
 
     return (
